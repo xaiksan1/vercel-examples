304da226e366e22b9addc17cf59ef2a800d69af9
Enias Cailliau <enias@steamship.com>
2023-03-01 13:33:15 +0100

    Add support for ingesting multiple books into a single index

--- ask-my-book-chatbot/steamship/ingest.py
+++ ask-my-book-chatbot/steamship/ingest.py
@@ -1,17 +1,33 @@
 """Script to populate the VectorDB."""
+import json
 
 from langchain.document_loaders import PagedPDFSplitter
 from steamship import Steamship
 from steamship_langchain.vectorstores import SteamshipVectorStore
 
-index_name = "ask-naval-ravikant-2"
-client = Steamship(workspace=index_name)
+INDEX_NAME = "your-index-name"
 
-loader = PagedPDFSplitter("uploads/the-almanack-of-naval-ravikant.pdf")
-pages = loader.load_and_split()
+BOOKS = [
+    "uploads/the-almanack-of-naval-ravikant.pdf",
+]
 
-doc_index = SteamshipVectorStore.from_texts(client=client,
-                                            texts=[page.page_content for page in pages],
-                                            metadatas=[page.metadata for page in pages],
-                                            index_name=index_name,
-                                            embedding="text-embedding-ada-002")
+client = Steamship(workspace=INDEX_NAME)
+
+doc_index = SteamshipVectorStore(client=client,
+                                 index_name=INDEX_NAME,
+                                 embedding="text-embedding-ada-002")
+
+books = set(json.loads(item.metadata)["source"] for item in doc_index.index.index.list_items().items)
+
+if books.intersection(set(books)):
+    print("Attempting to add the same book twice into the index with name {}.")
+    doc_index.index.reset()
+
+for book in BOOKS:
+    loader = PagedPDFSplitter(book)
+    pages = loader.load_and_split()
+
+    doc_index.add_texts(
+        texts=[page.page_content for page in pages],
+        metadatas=[page.metadata for page in pages],
+    )
